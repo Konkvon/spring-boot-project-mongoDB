61c7869570e8bc489ababa4c60095367370bca56
João Pedro <joaopedroorem@gmail.com>
2026-04-24 22:39:23 -0300

    Instanciação de Usuários

--- src/main/java/com/springboot/mongodb/entities/User.java
+++ src/main/java/com/springboot/mongodb/entities/User.java
@@ -3,7 +3,6 @@ package com.springboot.mongodb.entities;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.annotation.processing.Generated;
 import java.io.Serializable;
 import java.util.Objects;
 
