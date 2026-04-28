84d7b060af499b5616a1cfce4d9a8b2319371342
João Pedro <joaopedroorem@gmail.com>
2026-04-28 03:52:36 -0300

    Consulta simples com query methods

--- src/main/java/com/springboot/mongodb/repositories/PostRepository.java
+++ src/main/java/com/springboot/mongodb/repositories/PostRepository.java
@@ -3,5 +3,9 @@ package com.springboot.mongodb.repositories;
 import com.springboot.mongodb.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
 }
