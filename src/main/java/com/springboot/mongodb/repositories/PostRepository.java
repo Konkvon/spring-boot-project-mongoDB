9afc6a4396a01271b99bbcca0a548e015826ec7b
João Pedro <joaopedroorem@gmail.com>
2026-04-28 04:03:45 -0300

    Consulta simples com @Query

--- src/main/java/com/springboot/mongodb/repositories/PostRepository.java
+++ src/main/java/com/springboot/mongodb/repositories/PostRepository.java
@@ -2,10 +2,15 @@ package com.springboot.mongodb.repositories;
 
 import com.springboot.mongodb.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/springboot/mongodb/resources/PostResource.java
+++ src/main/java/com/springboot/mongodb/resources/PostResource.java
@@ -21,7 +21,7 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @GetMapping(value = "/titleserch")
+    @GetMapping(value = "/titlesearch")
     public  ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "")String text){
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
